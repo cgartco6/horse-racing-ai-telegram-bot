f6a4dcf5482204e3aac9063dbca9a201e3bdd63a
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-06-27 21:21:19 +0200

    Update self_learning.py

--- models/self_learning.py
+++ models/self_learning.py
@@ -1,16 +1,18 @@
 import pandas as pd
+import numpy as np
 from .prediction_model import RacingModel
 from config.settings import MIN_TRAINING_SAMPLES
+from services.data_loader import load_historical_data, save_historical_data
 
 class LearningSystem:
     def __init__(self):
         self.model = RacingModel()
-        self.historical_data = pd.read_csv('data/historical_results.csv')
+        self.historical_data = load_historical_data()
         
     def update_model(self, new_results):
         # Append new results to historical data
         updated_data = pd.concat([self.historical_data, new_results])
-        updated_data.to_csv('data/historical_results.csv', index=False)
+        save_historical_data(updated_data)
         
         # Retrain if enough new samples
         if len(updated_data) > MIN_TRAINING_SAMPLES:
@@ -18,7 +20,10 @@ class LearningSystem:
             
     def analyze_failures(self):
         # Analyze prediction errors to improve feature engineering
-        predictions = self.historical_data[self.historical_data['predicted']]
-        errors = predictions[predictions['outcome'] != predictions['predicted_outcome']]
-        # Implementation of error analysis logic
-        return error_insights
+        if 'predicted_outcome' in self.historical_data.columns:
+            predictions = self.historical_data[self.historical_data['predicted_outcome'].notna()]
+            errors = predictions[predictions['outcome'] != predictions['predicted_outcome']]
+            # Here we can add logic to analyze errors and adjust feature weights or add new features
+            return errors
+        else:
+            return pd.DataFrame()
